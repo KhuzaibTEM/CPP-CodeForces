98b803eb3ce397d43e3fbe49ee7b388d6e9d21f1
Khuzaib <79333922+KhuzaibTEM@users.noreply.github.com>
2024-05-31 23:20:37 +0500

    Create A. Turtle and Piggy Are Playing a Game.cpp

---
+++ A. Turtle and Piggy Are Playing a Game.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define REP(i,a,b) for (int i = a; i <= b; i++)
+#define elif else if
+#define lb lower_bound
+#define ub upper_bound
+
+//#define or ||          \\                //    ||    ||    \\    //
+//#define and &&          \\      /\      //     ||    ||     \\  //
+//                         \\    //\\    //      ||____||      \\//
+//                          \\  //  \\  //       ||____||       //
+//                           \\//    \\//        ||    ||      //
+//                            \/      \/         ||    ||     //
+
+typedef string str;
+typedef long long ll;
+typedef long double ld;
+typedef vector<int> vti;
+typedef vector<str> vts;
+
+int solve(){
+    return 0;
+}
+
+int main(){
+    int lim,l,r,count,max;
+    count = 1;
+    cin >> lim;
+    REP(i,1,lim) {
+        cin >> l;
+        cin >> r;
+        cout << int(log(r)) << endl;
+    }
+}
