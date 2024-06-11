7cc331dffcce464936327c6510ce71b4dd5e299e
Khuzaib <79333922+KhuzaibTEM@users.noreply.github.com>
2024-06-11 22:12:25 +0500

    Create B. Maximum Multiple Sum.cpp

---
+++ B. Maximum Multiple Sum.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define REP(i,a,b) for (int i = a; i <= b; i++)
+#define REV(i,a,b) for (int i = a; i >= b; i--)
+#define elif else if
+#define lb lower_bound
+#define ub upper_bound
+
+//#define or ||          \\                //    ||    ||    \\    //
+//#define and &&          \\      /\      //     ||    ||     \\  //
+//                         \\    //\\    //      ||____||      \\//
+//                          \\  //  \\  //       ||____||       //
+//                           \\//    \\//        ||    ||      //
+//                            \/      \/         ||    ||     //
+
+typedef string str;
+typedef long long ll;
+typedef long double ld;
+typedef vector<int> vti;
+typedef vector<str> vts;
+
+
+
+int solve() {
+    return 0;
+}
+
+int main() {
+    int lim;
+    cin >> lim;
+    REP(i,1,lim) {
+        ll num;
+        cin >> num;
+        if (num == 3) cout << 3 << endl;
+        else cout << 2 << endl;
+    }
+}
