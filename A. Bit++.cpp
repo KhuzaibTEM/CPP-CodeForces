fae5d60774471b4a3c958f80d889c2f2394932a9
Khuzaib <79333922+KhuzaibTEM@users.noreply.github.com>
2024-07-01 23:43:44 +0500

    Create A. Bit++.cpp

---
+++ A. Bit++.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define REP(i,a,b) for (ll i = a; i <= b; i++)
+#define REV(i,a,b) for (ll i = a; i >= b; i--)
+#define elif else if
+#define lb lower_bound
+#define ub upper_bound
+ 
+//#define or ||          \\                //    ||    ||    \\    //
+//#define and &&          \\      /\      //     ||    ||     \\  //
+//                         \\    //\\    //      ||____||      \\//
+//                          \\  //  \\  //       ||____||       //
+//                           \\//    \\//        ||    ||      //
+//                            \/      \/         ||    ||     //
+ 
+typedef string str;
+typedef long long ll;
+typedef long double ld;
+typedef vector<int> vti;
+typedef vector<str> vts;
+
+
+int main() {
+    int lim;
+    ll total = 0;
+    cin >> lim;
+    REP(i,1,lim) {
+        str temp;
+        cin >> temp;
+        if (temp == "X++" || temp == "++X") total++;
+        else total--;
+    }
+    cout << total << endl;
+}
