066e50df4f9c9a132acf465c9354b7839082db2b
Khuzaib <79333922+KhuzaibTEM@users.noreply.github.com>
2024-07-03 20:19:36 +0500

    Create A. Odd One Out.cpp

---
+++ A. Odd One Out.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define REP(i,a,b) for (ll i = a; i <= b; i++)
+#define REV(i,a,b) for (ll i = a; i >= b; i--)
+#define elif else if
+#define lb lower_bound
+#define ub upper_bound
+ 
+//#define or ||          \\                //    ||    ||    \\    //
+//#define and &&          \\      /\      //     ||    ||     \\  //
+//                         \\    //\\    //      ||____||      \\//
+//                          \\  //  \\  //       ||____||       //
+//                           \\//    \\//        ||    ||      //
+//                            \/      \/         ||    ||     //
+ 
+typedef string str;
+typedef long long ll;
+typedef long double ld;
+typedef vector<int> vti;
+typedef vector<str> vts;
+ 
+ 
+int main() {
+    int lim;
+    cin >> lim;
+    REP(i,1,lim) {
+        ll a,b,c;
+        cin >> a >> b >> c;
+        if (a == b) cout << c << endl;
+        elif (a == c) cout << b << endl;
+        else cout << a << endl;
+    }
+}
