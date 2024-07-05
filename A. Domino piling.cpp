e61aafd9a03bc87c66322a7e802514a71268435c
Khuzaib <79333922+KhuzaibTEM@users.noreply.github.com>
2024-07-05 23:55:54 +0500

    Create A. Domino piling.cpp

---
+++ A. Domino piling.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define REP(i,a,b) for (ll i = a; i <= b; i++)
+#define REV(i,a,b) for (ll i = a; i >= b; i--)
+#define elif else if
+#define lb lower_bound
+#define ub upper_bound
+ 
+//#define or ||          \\                //    ||    ||    \\    //
+//#define and &&          \\      /\      //     ||    ||     \\  //
+//                         \\    //\\    //      ||____||      \\//
+//                          \\  //  \\  //       ||____||       //
+//                           \\//    \\//        ||    ||      //
+//                            \/      \/         ||    ||     //
+ 
+typedef string str;
+typedef long long ll;
+typedef long double ld;
+typedef vector<int> vti;
+typedef vector<str> vts;
+
+
+int main(){
+    ll n,m;
+    cin >> n >> m;
+    cout << (n * m) / 2 << endl;
+}
